7bcf13acf2e38585b265872204ce89b72c555791
alirezatk <alirezatk73@gmail.com>
2020-12-22 22:59:35 +0300

    removed eslint

--- src/components/Slider/SlideShow.js
+++ src/components/Slider/SlideShow.js
@@ -20,6 +20,7 @@ const SlideShow = (props) => {
       clearInterval(timeInterval);
       clearInterval(sliderInterval);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function moveSlideArrows(type) {
